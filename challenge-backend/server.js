6e6d30c8a110ebf710fc4eec9a399f037e47ca2e
tavares97 <afpt.digital2013@gmail.com>
2021-06-05 22:51:56 +0100

    Server challenge testing

---
+++ challenge-backend/server.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const Joi = require('@hapi/joi');
+const queries = require('./queries');
+
+const init = async () => {
+	const server = Hapi.server({
+		port: 3000,
+		host: 'localhost',
+	});
+
+	server.route({
+		method: 'GET',
+		path: '/',
+		handler: (req, res) => {
+			const filter = req.query.filter;
+
+			return queries.getFilter(filter);
+		},
+	});
+
+	server.route({
+		method: 'POST',
+		path: '/tasks',
+		handler: (req, h) => {
+			const payload = req.payload;
+			const query = queries.insertUser(payload);
+
+			return query;
+		},
+	});
+
+	server.route({
+		method: 'PATCH',
+		path: '/tasks/{id}',
+		handler: (req, h) => {
+			const id = req.params.id;
+			const payload = req.payload;
+			const query = queries.updateTask(id, payload);
+
+			return query;
+		},
+	});
+
+	server.route({
+		method: 'DELETE',
+		path: '/tasks/{id}',
+		handler: (req, h) => {
+			const id = req.params.id;
+			const query = queries.deleteTask(id);
+
+			return query;
+		},
+	});
+
+	await server.start();
+	console.log(`Server running on: ${server.info.uri} `);
+};
+
+process.on('unhandledRejection', err => {
+	console.log(err);
+	process.exit(1);
+});
+
+init();
